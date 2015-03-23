9961282010842c9bdcdd890d2a0d03180016c92c
sherri <sherri@sherricruz.com>
2015-03-23 12:27:44 -0700

    added programs with api key

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 // An Angular module is a collection of functions that run when the app is booted.
+
+
+var apiKey = "MDAxMjk1ODg3MDEyOTc4NzE3NDE2MmE5Yg001",
+    nprUrl= 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON'
 var app = angular.module('myApp', []);
+app.controller('PlayerController', function($scope, $http) {
+
 
 //root controller
-app.controller('PlayerController', ['$scope', function($scope) {
+//app.controller('PlayerController', ['$scope', function($scope) {
+    /*
     $scope.playing = false;
     $scope.audio = document.createElement('audio');
     $scope.audio.src = '/media/npr.mp4';
@@ -25,3 +32,22 @@ app.controller('RelatedController', ['$scope', function ($scope){
 $scope.audio = documnet.createElement('audio');
     $scope.audio.src = '/media/npr.mp4';
 }]);
+    */
+$http({
+    method: 'JSONP',
+    url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+}).success(function(data, status) {
+    // Now we have a list of the stories (data.list.story)
+    // in the data object that the NPR API
+    // returns in JSON that looks like:
+    // data: { "list": {
+    //   "title": ...
+    //   "story": [
+    //     { "id": ...
+    //       "title": ...
+    //bind the list of clips to the $scope obj by storing list on $scope in the success callback
+    $scope.programs = data.list.story;
+}).error(function(data, status) {
+    // Some error occurred
+});
+});
