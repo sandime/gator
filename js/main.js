e3957d67dc34dba81541369f8e3e569dba7bb884
sherri <sherri@sherricruz.com>
2015-03-23 11:00:30 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// An Angular module is a collection of functions that run when the app is booted.
+var app = angular.module('myApp', []);
+
+//root controller
+app.controller('PlayerController', ['$scope', function ($scope) {
+
+}]);
+//controller fetches prgm listings
+app.controller('RelatedController', ['$scope', function ($scope){
+
+}]);
