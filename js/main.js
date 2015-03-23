87a064f85895e384fda4e24f60ce1a1c4011f084
sherri <sherri@sherricruz.com>
2015-03-23 11:17:58 -0700

    added functionality to controller

--- js/main.js
+++ js/main.js
@@ -2,10 +2,26 @@
 var app = angular.module('myApp', []);
 
 //root controller
-app.controller('PlayerController', ['$scope', function ($scope) {
-
+app.controller('PlayerController', ['$scope', function($scope) {
+    $scope.playing = false;
+    $scope.audio = document.createElement('audio');
+    $scope.audio.src = '/media/npr.mp4';
+    $scope.play = function() {
+        $scope.audio.play();
+        $scope.playing = true;
+    };
+    $scope.stop = function() {
+        $scope.audio.pause();
+        $scope.playing = false;
+    };
+    $scope.audio.addEventListener('ended', function() {
+        $scope.$apply(function() {
+            $scope.stop()
+        });
+    });
 }]);
 //controller fetches prgm listings
 app.controller('RelatedController', ['$scope', function ($scope){
-
+$scope.audio = documnet.createElement('audio');
+    $scope.audio.src = '/media/npr.mp4';
 }]);
